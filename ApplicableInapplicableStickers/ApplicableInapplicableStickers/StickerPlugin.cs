0ec565ba45d21f991d06bef4e5de6becf1d02469
Denyscrasav4ik <horanden238@gmail.com>
2026-09-09 19:36:33 +0300

    Initial Commit/Release

---
+++ ApplicableInapplicableStickers/ApplicableInapplicableStickers/StickerPlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ApplicableInapplicableStickers;
+
+[BepInPlugin("denyscrasav4ik.thedumbfactory.applicableinapplicablestickers", "Applicable Inapplicable Stickers", "1.0.0")]
+public class UnrestrictedStickersPlugin : BaseUnityPlugin
+{
+    private void Awake() => new Harmony("denyscrasav4ik.thedumbfactory.applicableinapplicablestickers").PatchAll();
+}
+
+[HarmonyPatch(typeof(StickerManager))]
+public static class StickerManagerPatches
+{
+    [HarmonyPatch("StickerCanBeApplied")]
+    [HarmonyPrefix]
+    internal static bool PrefixStickerCanBeApplied(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+
+    [HarmonyPatch("StickerCanBeCovered")]
+    [HarmonyPrefix]
+    internal static bool PrefixStickerCanBeCovered(ref bool __result)
+    {
+        __result = true;
+        return false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Baldi's Basics Plus Applicable Inapplicable Stickers
+
+## 📝 Functionality
+
+Basically, this mod makes the unappliable stickers applicable.
+
+## 📜 License
+
+This project is shared under the MIT License. Read LICENSE for more info.
